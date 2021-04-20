9dc01d43d54da033a97f442deb8c0e79bc747327
levis <levisouuza@gmail.com>
2021-04-20 07:41:51 -0300

    add projeto MovimentaBanco

---
+++ MovimentaBanco/MovimentaBanco/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace MovimentaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************");
+            Console.WriteLine(" Auto Atendimento Financeiro ");
+            Console.WriteLine("*****************************");
+
+            Console.WriteLine("\nCadastro Cliente:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Documento: ");
+            string doc = Console.ReadLine();
+            Console.Write("Valor em conta: R$ ");
+            double valor_conta = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Transacoes trans = new Transacoes(nome, doc, valor_conta);
+
+            Console.WriteLine("\nQual operação deseja realizar ?");
+
+            Console.WriteLine("1.Saque\n2.Depósito\n3.Consultar Saldo\n4.Finalizar Atendimento");
+            Console.Write("Opção: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (opcao > 4)
+                {
+                    while (opcao >= 4)
+                    {
+                        Console.Write("Informe uma opção válida: ");                        
+                        opcao = int.Parse(Console.ReadLine());
+                    }
+
+                }
+
+                else if (opcao == 4)
+                {   
+                    Console.WriteLine("\nFinalizando Atendimento. Volte sempre :)");
+                    break;
+                }
+
+                else if (opcao == 3)
+                {                   
+                    Console.WriteLine("\n" + trans);
+                }
+
+                else if (opcao == 1)
+                {
+                    Console.Write("\nInforme os valores dos Saques: ");
+                    string saques = Console.ReadLine();
+
+                    trans.Saque(saques);
+                    
+                    Console.WriteLine("Saques Realizados com sucesso!");
+                }
+
+
+                else if (opcao == 2)
+                {
+                    Console.Write("\nInforme os valores dos Depositos: ");
+                    string depositos = Console.ReadLine();
+
+                    trans.Deposito(depositos);
+
+                    Console.WriteLine("Depositos Realizados com sucesso!");
+                }
+
+                Console.WriteLine("\nDeseja realizar outra operação?");
+                Console.WriteLine("1.Saque\n2.Depósito\n3.Consultar Saldo\n4.Finalizar Atendimento");
+                Console.Write("Opção: ");
+                opcao = int.Parse(Console.ReadLine());
+            }
+
+            
+
+        }
+    }
+}
---
+++ MovimentaBanco/MovimentaBanco/Transacoes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace MovimentaBanco
+{
+    class Transacoes
+    {
+        public string Nome { get; private set; }
+        public string Documento { get; private set; }
+
+        public double ValorConta { get; private set; }
+
+        public Transacoes(string nome, string documento, double valor_conta)
+        {
+            Nome = nome;
+            Documento = documento;
+            ValorConta = valor_conta;
+        }
+
+        public void Saque(string valores)
+        {
+            var list_saques = valores.Split(" ");
+
+            for (int i = 0; i < list_saques.Length; i++)
+            {                
+                ValorConta -= Convert.ToDouble(list_saques[i]);
+
+            }
+
+        }
+
+        public void Deposito(string valores)
+        {
+            var list_depo = valores.Split(" ");
+
+            for (int i = 0; i < list_depo.Length; i++)
+            {
+                ValorConta += Convert.ToDouble(list_depo[i]);
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "Cliente: " + Nome + 
+                   ", Documento: " + Documento + 
+                   ", Saldo Atual: " + ValorConta.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+    }
+}
